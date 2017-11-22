157c8a25207b22130feb52555dda2bfe3f4a0429
Steven Fackler <sfackler@gmail.com>
2017-11-21 22:04:00 -0800

    Docs and stuff

--- rstack-self/src/lib.rs
+++ rstack-self/src/lib.rs
@@ -1,3 +1,42 @@
+//! Retrieve stack traces of all threads of the process.
+//!
+//! This is implemented using the [rstack] crate, which itself uses [libunwind]'s ability to
+//! trace remote processes using ptrace. Because processes cannot ptrace themselves, we're forced to
+//! use spawn a child process which does that work.
+//!
+//! [rstack]: https://sfackler.github.io/rstack/doc/rstack
+//! [libunwind]: http://www.nongnu.org/libunwind/
+//!
+//! # Example
+//!
+//! ```
+//! extern crate rstack_self;
+//!
+//! use std::env;
+//! use std::process::Command;
+//! use std::thread;
+//!
+//! fn main() {
+//!     if env::args_os().count() > 1 {
+//!         let _ = rstack_self::child();
+//!         return;
+//!     }
+//!
+//!     // spawn a second thread just for fun
+//!     thread::spawn(background_thread);
+//!
+//!     let exe = env::current_exe().unwrap();
+//!     let trace = rstack_self::trace(Command::new(exe).arg("child")).unwrap();
+//!
+//!     println!("{:#?}", trace);
+//! }
+//!
+//! fn background_thread() {
+//!     loop {
+//!         thread::park();
+//!     }
+//! }
+//! ```
 extern crate antidote;
 extern crate backtrace;
 extern crate bincode;
@@ -14,15 +53,42 @@ extern crate env_logger;
 
 use antidote::Mutex;
 use libc::{c_ulong, getppid, prctl, PR_SET_PTRACER};
-use std::process::{Command, Stdio};
-use std::error::Error;
-use std::path::{Path, PathBuf};
+use std::error;
+use std::fmt;
 use std::io::{self, BufReader, Read, Write};
+use std::path::{Path, PathBuf};
+use std::process::{Command, Stdio};
+use std::result;
 
 lazy_static! {
     static ref TRACE_LOCK: Mutex<()> = Mutex::new(());
 }
 
+/// The result type returned by methods in this crate.
+pub type Result<T> = result::Result<T, Error>;
+
+/// The error type returned by methods in this crate.
+#[derive(Debug)]
+pub struct Error(Box<error::Error + Sync + Send>);
+
+impl fmt::Display for Error {
+    fn fmt(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
+        fmt::Display::fmt(&self.0, fmt)
+    }
+}
+
+impl error::Error for Error {
+    fn description(&self) -> &str {
+        error::Error::description(&*self.0)
+    }
+
+    fn cause(&self) -> Option<&error::Error> {
+        error::Error::cause(&*self.0)
+    }
+}
+
+/// Information about a thread.
+#[derive(Debug, Clone)]
 pub struct Thread {
     id: u32,
     name: String,
@@ -30,34 +96,45 @@ pub struct Thread {
 }
 
 impl Thread {
+    /// Returns the thread's ID.
     pub fn id(&self) -> u32 {
         self.id
     }
 
+    /// Returns the thread's name.
     pub fn name(&self) -> &str {
         &self.name
     }
 
+    /// Returns the stack frames of the thread.
     pub fn frames(&self) -> &[Frame] {
         &self.frames
     }
 }
 
+/// Information about a stack frame.
+#[derive(Debug, Clone)]
 pub struct Frame {
     ip: usize,
     symbols: Vec<Symbol>,
 }
 
 impl Frame {
+    /// Returns the instruction pointer of the frame.
     pub fn ip(&self) -> usize {
         self.ip
     }
 
+    /// Returns the symbols resolved to this frame.
+    ///
+    /// Multiple symbols can be returned due to inlining.
     pub fn symbols(&self) -> &[Symbol] {
         &self.symbols
     }
 }
 
+/// Information about a symbol.
+#[derive(Debug, Clone)]
 pub struct Symbol {
     name: Option<String>,
     file: Option<PathBuf>,
@@ -65,44 +142,57 @@ pub struct Symbol {
 }
 
 impl Symbol {
+    /// Returns the name of the symbol, if known.
     pub fn name(&self) -> Option<&str> {
         self.name.as_ref().map(|n| &**n)
     }
 
+    /// Returns the file in which this symbol is defined, if known.
     pub fn file(&self) -> Option<&Path> {
         self.file.as_ref().map(|f| &**f)
     }
 
+    /// Returns the line at which the address which resolved to this symbol corresponds, if known.
     pub fn line(&self) -> Option<u32> {
         self.line
     }
 }
 
-pub fn trace(child: &mut Command) -> Result<Vec<Thread>, Box<Error + Sync + Send>> {
+/// Returns stack traces of all of the threads the calling process.
+///
+/// The provided `Command` should be configured to spawn a process which will call the [`child`]
+/// function. It must not use standard input or standard output, but standard error will be
+/// inherited and can be used. The spawned process must "directly" call `child` rather than
+/// spawning another process to call it. That is, the parent of the process that calls `child` is
+/// the one that will be traced.
+///
+/// [`child`]: fn.child.html
+pub fn trace(child: &mut Command) -> Result<Vec<Thread>> {
     let _guard = TRACE_LOCK.lock();
 
     let mut child = child
         .stdin(Stdio::piped())
         .stdout(Stdio::piped())
         .stderr(Stdio::inherit())
-        .spawn()?;
+        .spawn()
+        .map_err(|e| Error(e.into()))?;
 
     let mut stdin = child.stdin.take().unwrap();
     let mut stdout = BufReader::new(child.stdout.take().unwrap());
 
-    set_ptracer(child.id())?;
+    set_ptracer(child.id()).map_err(|e| Error(e.into()))?;
     let mut bomb = PtracerBomb(true);
 
-    stdin.write_all(&[0])?;
+    stdin.write_all(&[0]).map_err(|e| Error(e.into()))?;
 
-    let raw: Result<Vec<RawThread>, String> =
-        bincode::deserialize_from(&mut stdout, bincode::Infinite)?;
-    let raw = raw?;
+    let raw: result::Result<Vec<RawThread>, String> =
+        bincode::deserialize_from(&mut stdout, bincode::Infinite).map_err(|e| Error(e.into()))?;
+    let raw = raw.map_err(|e| Error(e.into()))?;
 
-    set_ptracer(0)?;
+    set_ptracer(0).map_err(|e| Error(Box::new(e)))?;
     bomb.0 = false;
 
-    stdin.write_all(&[0])?;
+    stdin.write_all(&[0]).map_err(|e| Error(Box::new(e)))?;
 
     let threads = symbolicate(raw);
 
@@ -179,21 +269,24 @@ struct RawFrame {
     is_signal: bool,
 }
 
-pub fn child() -> Result<(), Box<Error + Sync + Send>> {
+/// The function called by process spawned by a call to [`trace`].
+///
+/// [`trace`]: fn.trace.html
+pub fn child() -> Result<()> {
     let mut stdin = io::stdin();
     let mut stdout = io::stdout();
 
     let mut buf = [0];
-    stdin.read_exact(&mut buf)?;
+    stdin.read_exact(&mut buf).map_err(|e| Error(e.into()))?;
     let trace = child_trace();
-    bincode::serialize_into(&mut stdout, &trace, bincode::Infinite)?;
-    stdout.flush()?;
-    stdin.read_exact(&mut buf)?;
+    bincode::serialize_into(&mut stdout, &trace, bincode::Infinite).map_err(|e| Error(e.into()))?;
+    stdout.flush().map_err(|e| Error(e.into()))?;
+    stdin.read_exact(&mut buf).map_err(|e| Error(e.into()))?;
 
     Ok(())
 }
 
-fn child_trace() -> Result<Vec<RawThread>, String> {
+fn child_trace() -> result::Result<Vec<RawThread>, String> {
     let parent = unsafe { getppid() } as u32;
 
     match rstack::TraceOptions::new().thread_names(true).trace(parent) {
