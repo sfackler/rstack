07e9d3b01f06b3b4e6026938775ce287ceedac5c
Elia Geretto <elia.f.geretto@gmail.com>
2021-02-10 19:18:20 +0100

    Add test for x86_64 registers

--- unwind/tests/test.rs
+++ unwind/tests/test.rs
@@ -39,6 +39,63 @@ fn local() {
 }
 
 #[test]
+#[cfg(target_arch = "x86_64")]
+fn x86_64() {
+    macro_rules! dump_register {
+        ($cursor:ident, $reg_num:expr, $reg_name:literal) => {
+            if let Ok(reg_val) = $cursor.register($reg_num) {
+                println!("{}: {:#016x}", $reg_name, reg_val);
+            } else {
+                println!("{} not stored!", $reg_name);
+            }
+        };
+    }
+
+    fn bar() {
+        get_context!(context);
+        let mut cursor = Cursor::local(context).unwrap();
+
+        loop {
+            if let Ok(procedure_name) = cursor.procedure_name() {
+                println!("{}:", procedure_name.name());
+            } else {
+                println!("unknown:")
+            }
+
+            dump_register!(cursor, RegNum::RAX, "rax");
+            dump_register!(cursor, RegNum::RDX, "rdx");
+            dump_register!(cursor, RegNum::RCX, "rcx");
+            dump_register!(cursor, RegNum::RBX, "rbx");
+            dump_register!(cursor, RegNum::RSI, "rsi");
+            dump_register!(cursor, RegNum::RDI, "rdi");
+            dump_register!(cursor, RegNum::RBP, "rbp");
+            dump_register!(cursor, RegNum::RSP, "rsp");
+            dump_register!(cursor, RegNum::R8, "r8");
+            dump_register!(cursor, RegNum::R9, "r9");
+            dump_register!(cursor, RegNum::R10, "r10");
+            dump_register!(cursor, RegNum::R11, "r11");
+            dump_register!(cursor, RegNum::R12, "r12");
+            dump_register!(cursor, RegNum::R13, "r13");
+            dump_register!(cursor, RegNum::R14, "r14");
+            dump_register!(cursor, RegNum::R15, "r15");
+            dump_register!(cursor, RegNum::RIP, "rip");
+            dump_register!(cursor, RegNum::CFA, "cfa");
+            println!();
+
+            if !cursor.step().unwrap() {
+                break;
+            }
+        }
+    }
+
+    fn foo() {
+        bar();
+    }
+
+    foo();
+}
+
+#[test]
 #[cfg(feature = "ptrace")]
 fn remote() {
     use std::io;
